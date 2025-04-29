6bada337b346b6f455589a67110ef067003a2537
afshinkarampour <afshinkarampour@yahoo.com>
2025-04-29 11:46:07 -0700

    fix: listen on 0.0.0.0 for Docker deployment

--- backend/server.js
+++ backend/server.js
@@ -69,6 +69,6 @@ app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log("Server Started on PORT : " + port);
 });
