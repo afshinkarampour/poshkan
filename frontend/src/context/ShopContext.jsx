044962bcc4eae928dbf2fe928dca8570dd4d6571
afshinkarampour <afshinkarampour@yahoo.com>
2025-05-02 08:02:41 -0700

    edit axiosInstance in ShopContext file

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -2,7 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import axiosInstance from "../services/axiosinstance";
+import axiosInstance from "../services/axiosInstance";
 
 // import { products as dummyProducts } from "../assets/assets";
 
