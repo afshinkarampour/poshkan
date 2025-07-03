b8be130da2fb68f30bb2fe9f5f3f913b251afb72
afshinkarampour <afshinkarampour@yahoo.com>
2025-07-03 03:04:34 -0700

    scroll to top handle

--- frontend/src/components/ScrollToTop.jsx
+++ frontend/src/components/ScrollToTop.jsx
@@ -5,11 +5,15 @@ const ScrollToTop = () => {
   const { pathname } = useLocation();
 
   useEffect(() => {
+    console.log("go to top1");
+
     window.scrollTo({
       top: 0,
       left: 0,
       behavior: "smooth",
     });
+
+    console.log("go to top2");
   }, [pathname]);
 
   return null;
