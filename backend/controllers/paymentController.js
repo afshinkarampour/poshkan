7ccf734bb6bd907cb130a901a3b623c92d74fb46
afshinkarampour <afshinkarampour@yahoo.com>
2025-07-05 05:11:02 -0700

    bug fix

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -39,7 +39,7 @@ const paymentRequestSchema = Joi.object({
     address: Joi.string().optional(),
     email: Joi.string().email().optional(),
     province: Joi.string().max(25),
-    city: joi.string().max(30),
+    city: Joi.string().max(30),
   })
     .required()
     .messages({
